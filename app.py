ffd945742067b76928e7c9a7289fc41eb2df9728
Nathan <reusnat@bvu.edu>
2023-04-17 14:47:09 -0500

    added routes and basic login framework

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import (
     session, url_for
 )
 from flask_sqlalchemy import SQLAlchemy
-import os 
+import os
 
 app = Flask(__name__)
 app.secret_key = b'REPLACE_ME_x#pi*CO0@^z_beep_beep_boop_boop'
@@ -13,16 +13,60 @@ app.config['SQLALCHEMY_DATABASE_URI'] = sqlite_uri
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-from models import User
+
+def get_username():
+    if 'username' in session:
+        return session['username']
+
+    return None
+
+
+def authenticate(username, password):
+    if username == 'cartman' and password == 'beefcake':
+        session['username'] = username
+        return True
+
+    return False
+
+
+def is_secure_route(request):
+    return request.path not in ['/login/', '/logout/'] and \
+        not request.path.startswith('/static/')
+
+
+@app.before_request
+def login_redirect():
+    if get_username() is None and is_secure_route(request):
+        return redirect(url_for('login_form'))
+
 
 @app.route('/')
 def index():
-    return render_template('login.html')
+    return redirect(url_for('main'))
+
+
+@app.route('/main/')
+def main():
+    return render_template('main.html', username=get_username())
+
 
 @app.route('/login/', methods=['GET'])
-def get_login():
-    return render_template('login.html')
+def login_form():
+    return render_template('login_form.html')
+
 
 @app.route('/login/', methods=['POST'])
 def login():
-    return 'hi'
+    if authenticate(request.form['username'], request.form['password']):
+        return redirect(url_for('main'))
+    else:
+        return render_template('login_form.html',
+                               messages=['Invalid username/password'])
+
+
+@app.route('/logout/')
+def logout():
+    if 'username' in session:
+        del session['username']
+    return render_template('login_form.html',
+                           messages=['Logged out. Thanks for visiting.'])
